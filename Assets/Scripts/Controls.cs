4588aeba0133378ade276941e697d82dc1dbfe01
IIINZOIII <146147540+IIINZOIII@users.noreply.github.com>
2023-10-27 21:40:58 +1300

    Update 18(Enzo)

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controls : MonoBehaviour
+{
+    public void ControlScene()
+    {
+        SceneManager.LoadScene(5);
+    }
+}
