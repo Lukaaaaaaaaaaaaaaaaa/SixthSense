7a1aa87520e9c1760b1a5109e43ef1c3b333bf50
Lukaaaaaaaaaaaaaaaaa <lukabaz00kanz@gmail.com>
2023-10-27 23:40:40 +1300

    update

---
+++ Assets/TestAnim.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnim : MonoBehaviour
+{
+
+    public Moinster player;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+       animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<Moinster>().isMoving)
+        {
+            animator.SetBool("IsMoving", true);
+        }
+        else if(!player.GetComponent<Moinster>().isMoving)
+        {
+            animator.SetBool("IsMoving", false);
+        }
+    }
+}
